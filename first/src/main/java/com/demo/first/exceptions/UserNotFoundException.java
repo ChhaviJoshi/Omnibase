734ab8ea4d12ef277fb32f348d3a26cf34a286cd
Chhavi Joshi <chhavijoshi904@gmail.com>
2026-05-22 09:16:40 +0530

    user management project, repo and service layer updates, linking via jpa

--- first/src/main/java/com/demo/first/exceptions/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.demo.first.exceptions;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
