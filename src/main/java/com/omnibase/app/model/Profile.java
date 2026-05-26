c643e0d11787b5782a2253447beed9d99a1374e5
Chhavi Joshi <chhavijoshi904@gmail.com>
2026-05-26 08:09:15 +0530

    add fields, constructors, getters and setters

--- src/main/java/com/omnibase/app/model/Profile.java
+++ src/main/java/com/omnibase/app/model/Profile.java
@@ -1,5 +1,6 @@
 package com.omnibase.app.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 @Entity
@@ -8,7 +9,27 @@ public class Profile {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    private String bio;
+    private String phone;
+
     @OneToOne
     @JoinColumn(name = "user_id")
+    @JsonIgnore
     private User user;
+
+    public Profile() {
+    }
+
+    public Profile(String bio, String phone) {
+        this.bio = bio;
+        this.phone = phone;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
